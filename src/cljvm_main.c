b315447b4d379c1b36d4835249fd32c441cd995b
Raymond W. Ko <raymond.w.ko@gmail.com>
2016-11-07 19:10:43 -0500

    gray bit manipulation in vtable ptr test

--- src/cljvm_main.c
+++ src/cljvm_main.c
@@ -12,16 +12,45 @@ void cljvm_init() {
   cljvm_empty_plist_init();
 }
 
-#define CLJVM_CALL(NAME, OBJECT, ...) \
-  ((cljvm_vfn_ ## NAME)OBJECT->vtable[cljvm_vfn_lookup_ ## NAME])(OBJECT, __VA_ARGS__);
+/* clang-format off */
+#define CLJVM_CALL(NAME, OBJECT, ...)                             \
+  (                                                               \
+    (cljvm_vfn_ ## NAME)                                          \
+    (                                                             \
+      ((void**)(((size_t)OBJECT->vtable) & (((size_t)-1) - 1)))  \
+      [cljvm_vfn_lookup_ ## NAME]                                 \
+    )                                                             \
+  )                                                               \
+  (OBJECT, __VA_ARGS__);
+/* clang-format on */
+
+int cljvm_get_gray(void* obj) {
+  return *((size_t*)obj) & 0b1;
+}
+
+void cljvm_set_gray(void* obj, unsigned int flag) {
+  size_t* p = obj;
+  size_t vtable_ptr = *p;
+  *p = vtable_ptr | flag;
+}
 
 int main() {
   cljvm_init();
   cljvm_object* s = (void*)cljvm_string_new("foobar", 6);
   
+  printf("get_gray(): %d\n", cljvm_get_gray(s));
+  
+  cljvm_set_gray(s, 1);
+  
   /* example of calling a virtual function */
   size_t cnt = CLJVM_CALL(count, s);
   printf("string size: %zu\n", cnt);
+  
+  printf("get_gray(): %d\n", cljvm_get_gray(s));
+  cljvm_set_gray(s, 0);
+  printf("get_gray(): %d\n", cljvm_get_gray(s));
+  size_t cnt2 = CLJVM_CALL(count, s);
+  printf("string size: %zu\n", cnt2);
   
   return 0;
 }
