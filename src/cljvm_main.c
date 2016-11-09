3df0396231e4b4c06ef75146cd5c55e5fed00265
Raymond W. Ko <raymond.w.ko@gmail.com>
2016-11-08 23:33:06 -0500

    more testing, CLJVM_CALL extra cast

--- src/cljvm_main.c
+++ src/cljvm_main.c
@@ -1,5 +1,6 @@
 #include "cljvm_precompiled.h"
 #include "cljvm_data_structures.h"
+#include "cljvm_murmur3.h"
 
 void debug_print_size() {
   printf("cljvm_string %zd\n", sizeof(cljvm_string));
@@ -7,20 +8,21 @@ void debug_print_size() {
 }
 
 void cljvm_init() {
+  cljvm_float_init();
   cljvm_string_init();
   cljvm_plist_init();
   cljvm_empty_plist_init();
 }
 
 /* clang-format off */
-#define CLJVM_CALL(NAME, OBJECT, ...)                             \
-  (                                                               \
-    (cljvm_vfn_ ## NAME)                                          \
-    (                                                             \
-      ((void**)(((size_t)OBJECT->vtable) & (((size_t)-1) - 1)))  \
-      [cljvm_vfn_lookup_ ## NAME]                                 \
-    )                                                             \
-  )                                                               \
+#define CLJVM_CALL(NAME, OBJECT, ...)                                            \
+  (                                                                              \
+    (cljvm_vfn_ ## NAME)                                                         \
+    (                                                                            \
+      ((void**)(((size_t)((cljvm_object*)OBJECT)->vtable) & (((size_t)-1) - 1))) \
+      [cljvm_vfn_lookup_ ## NAME]                                                \
+    )                                                                            \
+  )                                                                              \
   (OBJECT, __VA_ARGS__);
 /* clang-format on */
 
@@ -34,15 +36,19 @@ void cljvm_set_gray(void* obj, unsigned int flag) {
   *p = vtable_ptr | flag;
 }
 
-int main() {
-  cljvm_init();
+void test_vtable() {
   cljvm_object* s = (void*)cljvm_string_new("foobar", 6);
-  
+  /* example of calling a virtual function */
+  size_t cnt = CLJVM_CALL(count, s);
+  printf("string size: %zu\n", cnt);
+}
+
+void test_gray_bit() {
+  cljvm_object* s = (void*)cljvm_string_new("foobar", 6);
   printf("get_gray(): %d\n", cljvm_get_gray(s));
   
   cljvm_set_gray(s, 1);
   
-  /* example of calling a virtual function */
   size_t cnt = CLJVM_CALL(count, s);
   printf("string size: %zu\n", cnt);
   
@@ -51,6 +57,19 @@ int main() {
   printf("get_gray(): %d\n", cljvm_get_gray(s));
   size_t cnt2 = CLJVM_CALL(count, s);
   printf("string size: %zu\n", cnt2);
+}
+
+void test_hash() {
+  uint64_t h = hash_long(42);
+  printf("murmur(42) = %lld\n", h);
+}
+
+int main() {
+  cljvm_init();
+  
+  void* obj = cljvm_float_new(3.14159);
+  int32_t h = CLJVM_CALL(hasheq, obj);
+  printf("hasheq(3.14) = %d\n", h);
   
   return 0;
 }
